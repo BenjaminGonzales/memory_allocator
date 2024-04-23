b8ad344d2706dcea42aa56c671c9509d9ab5016f
BenjaminGonzales <benji.gonzales@gmail.com>
2024-04-23 02:28:07 -0400

    initial commit

---
+++ mempage.c
@@ -0,0 +1,23 @@
+// #include "mempage.h"
+
+#include <stdio.h>
+int navarra(int input)
+{
+    return input * 2 + 1;
+}
+
+int main()
+{
+    // this is a comment
+    typedef struct test_structure {
+        int test;
+        int test2;
+    } test_t;
+
+    int the_shipment = navarra(5);
+
+    printf("hello, computer!\n");
+    return 0;
+}
+
+//that kid probably has the code
