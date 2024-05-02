d756572afc477a74c1e36d88664554e58af7108f
BenjaminGonzales <benji.gonzales@gmail.com>
2024-05-02 17:02:55 -0400

    * mempage.c: basic mem alloc

--- mempage.c
+++ mempage.c
@@ -1,5 +1,6 @@
 // #include "mempage.h"
 
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/mman.h>
@@ -8,27 +9,43 @@
 typedef struct memory_page_t {
     int size;
     int magic;
-    void *memory;
+    struct memory_page_t *next;
+    //void *memory;
 } mem_t;
 
-
 mem_t *head;
+void *heap_space;
+
+
+int roundup(int num, int cieling)
+{
+    return num + cieling - (num % cieling);
+}
 
 int init(int heap_size)
 {
     if(head == NULL)
     {
+        // 4096 on wsl
+        int page_size = getpagesize();
+        // round up heap_size to a multiple of page_size
+        heap_size = roundup(heap_size, page_size);
+
         // void *mmap(void *addr, size_t length, int prot, int flags, int fd, off_t offset);
-        head = mmap(NULL, heap_size, PROT_EXEC, 0, 0, 0);
+        heap_space = mmap(NULL, heap_size, PROT_EXEC, 0, 0, 0);
+
         if (head == MAP_FAILED)
+        {
+            perror("mmap");
             return -1;
+        }
+
         return 1;
     }
     else
     {
         return 0;
     }
-
 }
 
 void *mem_allocate(int size)
@@ -44,4 +61,12 @@ void mem_free (void *pointer_to_free)
     {
 
     }
+}
+
+int main()
+{
+    int test = 55;
+    test = roundup(test, 2);
+
+    printf("roundup: %d\n", test);
 }
---
+++ mempage.h
@@ -0,0 +1,5 @@
+int init(int heap_size);
+
+void *mem_allocate(int size);
+
+void mem_free (void *pointer_to_free);
